d3ce2404a688547263a8078e7ae64bbc692eec20
Hilal2013 <hilalozkul@gmail.com>
2023-04-17 08:45:31 -0400

    updated

--- src/main/java/com/cydeo/repository/RoleRepository.java
+++ src/main/java/com/cydeo/repository/RoleRepository.java
@@ -4,6 +4,8 @@ import com.cydeo.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role,Long> {
+//give me the role based on description
+    Role findByDescription(String description);
 
 
 }
