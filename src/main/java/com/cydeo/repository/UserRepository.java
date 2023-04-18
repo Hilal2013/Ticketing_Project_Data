a9bcbe6c8558b84e11c4019e0c9a3017bab94f14
Hilal2013 <hilalozkul@gmail.com>
2023-04-18 12:52:41 -0400

    updated

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -2,10 +2,17 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
     //get user based on username
     //derived query
     User findByUserName(String username);
+    @Transactional
+    void deleteByUserName(String username);
+    //maybe you deleted in the UI but not database/use transactional
+    //you can put on class level
+    //but dont forget not delete from database delete from UI
 }
