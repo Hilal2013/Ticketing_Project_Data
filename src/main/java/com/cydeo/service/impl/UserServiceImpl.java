540147b2497b628d015a25234f9b923d96914c10
Hilal2013 <hilalozkul@gmail.com>
2023-04-18 20:49:17 -0400

    updated

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -46,19 +46,33 @@ List<User> userList=userRepository.findAll(Sort.by("firstName"));
     }
 
     @Override
-    public void update(UserDTO user) {
+    public UserDTO update(UserDTO user) {
 //convert user dto to entity object//Find current user// set id
   User user1=   userRepository.findByUserName(userMapper.convertToEntity(user).getUserName());
       //set id of entity user to the converted object
 User convertedUser=userMapper.convertToEntity(user);
   convertedUser.setId(user1.getId());
 userRepository.save(convertedUser);
-
+return findByUserName(user.getUserName());
     }
+
+
+
     @Override
     public void deleteByUserName(String username) {
          userRepository.deleteByUserName(username);
 
     }
+    @Override
+    public void delete(String username) {
 
+        //go to db and get that user with username
+        //change the isdeleted field to true
+        //save the object in database
+        User user=userRepository.findByUserName(username);
+        user.setIsDeleted(true);
+        userRepository.save(user);
+
+
+    }
 }
