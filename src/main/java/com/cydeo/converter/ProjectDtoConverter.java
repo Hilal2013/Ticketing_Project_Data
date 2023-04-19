c8ace3d80ff426e515b1bddad04e09ce1aa90121
Hilal2013 <hilalozkul@gmail.com>
2023-04-19 11:17:20 -0400

    updated

--- src/main/java/com/cydeo/converter/ProjectDtoConverter.java
+++ src/main/java/com/cydeo/converter/ProjectDtoConverter.java
@@ -4,11 +4,12 @@ import com.cydeo.dto.ProjectDTO;
 
 import com.cydeo.service.ProjectService;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationPropertiesBinding
+//@ConfigurationPropertiesBinding
 public class ProjectDtoConverter implements Converter<String, ProjectDTO> {
 
     ProjectService projectService;
