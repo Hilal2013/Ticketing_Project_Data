83e0218b522f1bef503411b2de83f70730c8a338
Hilal2013 <hilalozkul@gmail.com>
2023-04-18 20:49:57 -0400

    updated

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -21,6 +21,8 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private Boolean isDeleted=false;
     private LocalDateTime insertDateTime;
     private Long insertUserId;
     private LocalDateTime lastUpdateDateTime;
