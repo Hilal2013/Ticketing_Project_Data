02b662b99a093e0462b2bf1128d53d7edca499b1
Hilal2013 <hilalozkul@gmail.com>
2023-04-18 21:30:48 -0400

    updated

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
@@ -23,9 +20,35 @@ public class BaseEntity {
     private Long id;
 
     private Boolean isDeleted=false;
+    @Column(nullable = false,updatable = false)
     private LocalDateTime insertDateTime;
+    @Column(nullable = false,updatable = false)
     private Long insertUserId;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
+
+
+    //this method needs to be executed whenever we create the object=>a new user/project/manager
+    @PrePersist//trying save//
+    private void onPrePersist(){
+        //I created  a user is gonna current time
+        this.insertDateTime = LocalDateTime.now();
+        //in the beginning is gonna be current time too
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.insertUserId=1L;
+        //security is not connected yet//this is now hard code
+        //when we start the security this will be connected with security
+        this.lastUpdateUserId=1L;
+    }
+    //this method executed whenever we update the object
+    @PreUpdate
+    private void onPreUpdate(){
+        //is gonna be update
+        this.lastUpdateDateTime=LocalDateTime.now();
+        //whoever is updating this user//is gonna take that primary key
+        this.lastUpdateUserId=1L;
+    }
 
 }
