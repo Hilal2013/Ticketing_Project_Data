9c1bc621ba3a5f5b765326c3d9059599faa5f144
Hilal2013 <hilalozkul@gmail.com>
2023-04-19 08:58:41 -0400

    updated

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,4 +1,52 @@
 package com.cydeo.service.impl;
 
-public class ProjectServiceImpl {
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.mapper.ProjectMapper;
+import com.cydeo.repository.ProjectRepository;
+import com.cydeo.service.ProjectService;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+    private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
+
+    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+        this.projectRepository = projectRepository;
+        this.projectMapper = projectMapper;
+    }
+
+    @Override
+    public ProjectDTO getByProjectCode(String code) {
+        return null;
+    }
+
+    @Override
+    public List<ProjectDTO> listAllProjects() {
+
+     return   projectRepository.findAll(Sort.by("projectCode")).stream()
+                .map(entity->projectMapper.convertToDto(entity))
+                .collect(Collectors.toList());
+
+
+    }
+
+    @Override
+    public void save(ProjectDTO dto) {
+
+    }
+
+    @Override
+    public void update(ProjectDTO dto) {
+
+    }
+
+    @Override
+    public void delete(ProjectDTO dto) {
+
+    }
 }
