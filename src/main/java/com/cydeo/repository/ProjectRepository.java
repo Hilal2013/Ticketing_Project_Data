268bd740fc8763ee1b10c61064c3b4c512c693c1
Hilal2013 <hilalozkul@gmail.com>
2023-04-19 08:58:35 -0400

    updated

--- src/main/java/com/cydeo/repository/ProjectRepository.java
+++ src/main/java/com/cydeo/repository/ProjectRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProjectRepository extends JpaRepository<Project,Long> {
 
     Project findByProjectCode(String code);
+
 }
 
