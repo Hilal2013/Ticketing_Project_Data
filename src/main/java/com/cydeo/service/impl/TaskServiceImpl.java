c095d67a00d52775aa9a0c2f17381a917e3efa56
Hilal2013 <hilalozkul@gmail.com>
2023-04-24 12:00:50 -0400

    updated

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,42 +1,76 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.entity.Task;
+import com.cydeo.enums.Status;
+import com.cydeo.mapper.TaskMapper;
 import com.cydeo.repository.TaskRepository;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 public class TaskServiceImpl implements TaskService {
 
-private final TaskRepository taskRepository;
+    private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
 
-    public TaskServiceImpl(TaskRepository taskRepository) {
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {
         this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
     }
 
     @Override
-    public List<TaskDTO> listAllTasks() {
-        return null;
+    public List<TaskDTO> listAllTasks() {//go to database and get all of them
+        return taskRepository.findAll().stream()
+                .map(taskMapper::convertToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public void save(TaskDTO task) {
-
+         task.setTaskStatus(Status.OPEN);
+        task.setAssignedDate(LocalDate.now());
+        taskRepository.save(taskMapper.convertToEntity(task));
     }
 
-    @Override
-    public void update(TaskDTO task) {
-
-    }
 
     @Override
     public void delete(Long id) {
 
+       Optional<Task>  foundTask=taskRepository.findById(id);
+if(foundTask.isPresent()){
+    foundTask.get().setIsDeleted(true);
+    taskRepository.save(foundTask.get());
+}
     }
 
     @Override
     public TaskDTO findById(Long id) {
+        //return taskMapper.convertToDto(taskRepository.findById(id).get());
+
+        Optional<Task> task = taskRepository.findById(id);
+
+        if(task.isPresent()){
+            return taskMapper.convertToDto(task.get());
+        }
         return null;
+    }
+
+    @Override
+    public void update(TaskDTO dto) {
+Optional<Task> task=taskRepository.findById(dto.getId());
+Task convertedTask =taskMapper.convertToEntity(dto);
+if(task.isPresent()){// already setId in Controller
+    convertedTask.setTaskStatus(task.get().getTaskStatus());
+   //while we are saving in the form task status and assigneddate that are in the table
+    convertedTask.setAssignedDate(task.get().getAssignedDate());
+    taskRepository.save(convertedTask);
+}
+
     }
 }
