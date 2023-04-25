acfa6e60cee80ffe9fda1c0360728a539370e2be
Hilal2013 <hilalozkul@gmail.com>
2023-04-25 11:23:00 -0400

    updated

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,10 +1,15 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.TaskDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.User;
 import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.UserRepository;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -15,19 +20,23 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final ProjectService projectService;
+    private final TaskService taskService;
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, @Lazy ProjectService projectService, @Lazy TaskService taskService) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.projectService = projectService;
+        this.taskService = taskService;
     }
 
     @Override
     public List<UserDTO> listAllUsers() {
 
-List<User> userList=userRepository.findAll(Sort.by("firstName"));
- return userList.stream()
-         .map(entity->userMapper.convertToDto(entity))
-         .collect(Collectors.toList());
+        List<User> userList = userRepository.findAll(Sort.by("firstName"));
+        return userList.stream()
+                .map(entity -> userMapper.convertToDto(entity))
+                .collect(Collectors.toList());
 
     }
 
@@ -48,28 +57,28 @@ List<User> userList=userRepository.findAll(Sort.by("firstName"));
     @Override
     public UserDTO update(UserDTO user) {
 //convert user dto to entity object//Find current user// set id
-  User user1=   userRepository.findByUserName(userMapper.convertToEntity(user).getUserName());
-      //set id of entity user to the converted object
-User convertedUser=userMapper.convertToEntity(user);
-  convertedUser.setId(user1.getId());
-userRepository.save(convertedUser);
-return findByUserName(user.getUserName());
+        User user1 = userRepository.findByUserName(userMapper.convertToEntity(user).getUserName());
+        //set id of entity user to the converted object
+        User convertedUser = userMapper.convertToEntity(user);
+        convertedUser.setId(user1.getId());
+        userRepository.save(convertedUser);
+        return findByUserName(user.getUserName());
     }
-
 
 
     @Override
     public void deleteByUserName(String username) {
-         userRepository.deleteByUserName(username);
+        userRepository.deleteByUserName(username);
 
     }
+
     @Override
     public void delete(String username) {
 
         //go to db and get that user with username
         //change the isdeleted field to true
         //save the object in database
-        User user=userRepository.findByUserName(username);
+        User user = userRepository.findByUserName(username);
         user.setIsDeleted(true);
         userRepository.save(user);
 
@@ -81,7 +90,25 @@ return findByUserName(user.getUserName());
         //go to db //bring all users with spesific role//if I pass manager  is gonna bring manager users
 
         return userRepository.findByRoleDescriptionIgnoreCase(role).stream()
-                .map(entity->userMapper.convertToDto(entity))
+                .map(entity -> userMapper.convertToDto(entity))
                 .collect(Collectors.toList());
     }
+
+    private boolean checkIfUserCanBeDeleted(User user) {
+//if user manager or employee check it
+        switch (user.getRole().getDescription()) {
+            case "Manager":
+                List<ProjectDTO> projectDTOList = projectService.listAllNonCompletedByAssignedManager(userMapper.convertToDto(user));
+                return projectDTOList.size() == 0;
+            case "Employee":
+                List<TaskDTO> taskDTOList = taskService.listAllNonCompletedByAssignedEmployee(userMapper.convertToDto(user));
+                return taskDTOList.size() == 0;//all of them completed you can delete //true
+
+            default:
+                return true;
+        }
+
+    }
+
+
 }
