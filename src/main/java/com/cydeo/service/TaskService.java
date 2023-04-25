c64eed8b829cb8110e34d039f2be7a57c6e45f44
Hilal2013 <hilalozkul@gmail.com>
2023-04-24 22:18:10 -0400

    updated

--- src/main/java/com/cydeo/service/TaskService.java
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -1,7 +1,10 @@
 package com.cydeo.service;
 
+import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.Task;
+import com.cydeo.enums.Status;
 
 import java.util.List;
 
@@ -14,5 +17,13 @@ List<TaskDTO> listAllTasks();
     TaskDTO findById(Long id);
 int totalNonCompletedTask(String projectCode);
 int totalCompletedTask(String projectCode);
+  void   deleteByProject(ProjectDTO projectDTO);
+    void completeByProject(ProjectDTO projectDTO);
+
+    List<TaskDTO> listAllTasksByStatusIsNot(Status status);
+    List<TaskDTO> listAllTasksByStatus(Status status);
+
+    List<TaskDTO> listAllNonCompletedByAssignedEmployee(UserDTO assignedEmployee);
+
 
 }
