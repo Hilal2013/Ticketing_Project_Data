46a4b277673453ecf864ff5600ca87280e3a850c
Hilal2013 <hilalozkul@gmail.com>
2023-04-24 22:18:05 -0400

    updated

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,8 +1,12 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.Project;
 import com.cydeo.entity.Task;
 import com.cydeo.enums.Status;
+import com.cydeo.mapper.ProjectMapper;
 import com.cydeo.mapper.TaskMapper;
 import com.cydeo.repository.TaskRepository;
 import com.cydeo.service.TaskService;
@@ -18,10 +22,11 @@ public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
     private final TaskMapper taskMapper;
-
-    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {
+private final ProjectMapper projectMapper;
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper, ProjectMapper projectMapper) {
         this.taskRepository = taskRepository;
         this.taskMapper = taskMapper;
+        this.projectMapper = projectMapper;
     }
 
     @Override
@@ -83,6 +88,44 @@ if(task.isPresent()){// already setId in Controller
     @Override
     public int totalCompletedTask(String projectCode) {
         return taskRepository.totalCompletedTasks(projectCode);
+    }
+
+    @Override
+    public void deleteByProject(ProjectDTO projectDTO) {
+        Project project=projectMapper.convertToEntity(projectDTO);
+        //now find tasks belongs to project
+        List<Task> listTask=taskRepository.findAllByProject(project);
+        //i want to delete one by one
+        listTask.forEach(task -> delete(task.getId()));
+    }
+
+    @Override
+    public void completeByProject(ProjectDTO projectDTO) {
+        //bring project
+        Project project=projectMapper.convertToEntity(projectDTO);
+        //now find tasks belongs to project
+        List<Task> listTask=taskRepository.findAllByProject(project);
+     listTask.stream().map(taskMapper::convertToDto)
+             .forEach(taskDTO -> {
+         taskDTO.setTaskStatus(Status.COMPLETE);
+         update(taskDTO);
+     });
+
+    }
+
+    @Override
+    public List<TaskDTO> listAllTasksByStatusIsNot(Status status) {
+        return null;
+    }
+
+    @Override
+    public List<TaskDTO> listAllTasksByStatus(Status status) {
+        return null;
+    }
+
+    @Override
+    public List<TaskDTO> listAllNonCompletedByAssignedEmployee(UserDTO assignedEmployee) {
+        return null;
     }
 
 
